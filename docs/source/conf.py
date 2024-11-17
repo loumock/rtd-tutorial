b5bde88374757d5e5340272730325cd260111598
loumock <loumock@hotmail.fr>
2024-11-18 00:25:36 +0100

    projet 1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Introduction to IA IP project'
+copyright = '2024, A.Gayen, M.Longatte, L.Mocq & M.Ottavy'
+author = 'A.Gayen, M.Longatte, L.Mocq & M.Ottavy'
 
 release = '0.1'
 version = '0.1.0'
